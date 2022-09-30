d0d7c1c2e7f9e76f983dbf1d7c808ac841150e38
Srthk03 <107133383+Srthk03@users.noreply.github.com>
2022-09-30 21:25:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
+import aiml 
+import os
 
 
 
@@ -13,6 +15,7 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'secretkey'
 
 login_manager = LoginManager()
@@ -125,7 +128,40 @@ def feedbackForm():
 def payment():
     return render_template('payment.html')
 
+
+
+#ChatBot
+@app.route("/chat")
+@login_required
+def chat():
+    return render_template('chat.html')
+
+@app.route("/ask", methods=['GET','POST'])
+@login_required
+def ask():
+  message = request.form['messageText'].encode('utf-8').strip()
+
+  kernel = aiml.Kernel()
+
+  if os.path.isfile("bot_brain.brn"):
+      kernel.bootstrap(brainFile = "bot_brain.brn")
+  else:
+      kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+      kernel.saveBrain("bot_brain.brn")
+
+  
+  while True:
+      if message == "quit":
+          exit()
+      elif message == "save":
+          kernel.saveBrain("bot_brain.brn")
+      else:
+          bot_response = kernel.respond(message)
+          
+          return jsonify({'status':'OK','answer':bot_response})
+
+
 if __name__ == '__main__':
     app.run(debug=True)   
-
+    
 
